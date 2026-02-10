ffc5249d8b6d65d32d2cfb18dd0e3c70b47a3ec6
ljt <2402116369@qq.com>
2026-02-10 16:53:22 +0800

    自动注册逻辑编写。

---
+++ spring-manual-bean-registration/src/main/java/com/tt/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.tt.controller;
+
+import com.tt.service.UserService;
+
+/**
+ * UserController - 用户控制层
+ * 
+ * 注意：这个类没有使用任何 Spring 注解（如 @Controller）
+ * 它将在容器外部创建，然后通过 registerSingleton 或 registerBeanDefinition 手动注册到 Spring 容器中
+ */
+public class UserController {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void handle() {
+        userService.process();
+    }
+}
